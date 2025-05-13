416b93ac39faac800a67aedb8274be45cffe7976
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-05-13 12:06:11 -0500

    Ceacion de migraciones
    Models, Seeders y Factories

---
+++ orderweb/database/seeders/CausalSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Causal;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CausalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Causal::insert([
+            ['description'=>'Reparación contador'],
+            ['description'=>'Suspensión del servicio'],
+            ['description'=>'Reconexión del servicio'],
+            ['description'=>'Instalación del servicio'],
+            ['description'=>'Cambio del contador']
+        ]);
+    }
+}
